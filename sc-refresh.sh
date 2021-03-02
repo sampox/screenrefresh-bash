c4328bdbe57410cbfc4baa71ddc46bdb2666a552
sampox <sampofi@gmail.com>
2021-03-02 09:36:32 +0200

    Update sc-refresh.sh

--- sc-refresh.sh
+++ sc-refresh.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 xrandr --output DVI-0 --off; xrandr --output DVI-0 --auto;
+#Replace DVI-0 with the correct screen device as necessary –> you can see the options by running "xrandr" command without any arguments
